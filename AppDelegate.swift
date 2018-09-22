a9c317e3f2458c6f20e8ffc80b6cc0ac0769a446
ysuo85 <yi.suo@stonybrook.edu>
2018-09-22 10:40:05 -0400

    Modifies project to use connected colors storyboard and swift.

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -1,18 +1,9 @@
-//
-//  AppDelegate.swift
-//  SparklingNodes
-//
-//  Created by yi suo on 9/22/18.
-//  Copyright © 2018 Team SparklingNodes. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
